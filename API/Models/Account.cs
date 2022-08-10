3da8f79fc6352b5af8dc38ee3604dabc0453a2ca
Ranur-react <rahmatnur845@gmail.com>
2022-08-11 02:16:41 +0700

    made models

---
+++ API/Models/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace API.Models
+{
+    [Table("Tb_M_Accounts")]
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public String Password { get; set; }
+        [ForeignKey("Employee")]
+        [Required(ErrorMessage = "it must have a value")]
+        public String Username { get; set; }
+        public Status Status { get; set; }
+        public DateTime ExpiredToken { get; set; }
+        public DateTime Last_online { get; set; }
+        public String Avatar { get; set; }
+        public int OTP { get; set; }
+        public Boolean IsUsed { get; set; }
+        [ForeignKey("Role")]
+        public int RoleId { get; set; }
+
+        public virtual Role Role { get; set; }
+        [JsonIgnore]
+        public virtual Employee Employee { get; set; }
+    }
+    public enum Status
+    {
+        disable,
+        enable,
+        limited
+    }
+}
